8e73f9cef84667fb66753411c13f8dbd5d7d564a
alexanderhes <alex.hesselberg@hotmail.com>
2026-02-24 12:04:20 +0100

    Updated to match main branch changes

--- bin/R_summary.R
+++ bin/R_summary.R
@@ -59,7 +59,7 @@ combined_all$Species <- "M-koppevirus"
 
 combined_all <- combined_all %>% 
   rename(
-    SequenceID = sample_id,
+    LW_id = sample_id,
     Barcode = barcode, 
     Clade = clade, 
     Lineage = lineage, 
@@ -70,13 +70,13 @@ combined_all <- combined_all %>%
     MappedPercentage = Mapped_Percentage
   ) %>% 
   select(
-    -fastq, -PrøveNr, -Mapped_Reads_Unnormalized, -Mapped_Percentage_Unnormalized
+    -fastq, -Mapped_Reads_Unnormalized, -Mapped_Percentage_Unnormalized
   ) %>%
   # Adding the Sequence quality logic
   mutate(`Sequence quality` = case_when(
-    Coverage > 80  ~ "High Quality",
-    Coverage >= 50 & Coverage <= 80 ~ "Medium Quality",
-    Coverage < 50  ~ "Low Quality",
+    Coverage > 80  ~ "High Quality. Lineage assignment high confidence.",
+    Coverage >= 50 & Coverage <= 80 ~ "Medium Quality. Lineage assignment medium confidence.",
+    Coverage < 50  ~ "Low Quality. Lineage assignment low confidence.",
     TRUE           ~ "Unknown"
   ))
 
